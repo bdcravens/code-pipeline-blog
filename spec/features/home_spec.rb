db6272e5915105d98cca0518b5fff01c105a1de6
Billy Cravens <bdcravens@gmail.com>
2016-12-16 20:42:21 -0600

    simple rails app with basic tests

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.feature "Home page", :type => :feature do
+  scenario "User visits home page" do
+    visit "/"
+
+    expect(page).to have_text("Dashboard")
+  end
+end
